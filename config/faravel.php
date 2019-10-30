b3bdc53922d3e8de0ee55f28dbe4a23fdd04db50
李星星 <sss60@qq.com>
2019-10-30 09:39:03 +0800

    update

--- config/faravel.php
+++ config/faravel.php
@@ -4,4 +4,9 @@ return [
         'enable' => false,
         'log' => env('LOG_CHANNEL', 'stack')
     ],
+
+    'listen_redis' => [
+        'default' => env('LOG_CHANNEL', 'stack'),
+        'cache' => env('LOG_CHANNEL', 'stack'),
+    ],
 ];
--- src/FaravelServiceProvider.php
+++ src/FaravelServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Faravel;
 
 use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Redis\Events\CommandExecuted;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
@@ -28,6 +29,7 @@ class FaravelServiceProvider extends ServiceProvider
         $this->registerPredisex();
         $this->registerCommand();
         $this->listenSql();
+        $this->listenRedis();
     }
 
     /**
@@ -56,8 +58,23 @@ class FaravelServiceProvider extends ServiceProvider
         if(config('faravel.listen_sql.enable')) {
             DB::listen(function (QueryExecuted $query) {
                 $sql = sql($query->sql, $query->bindings);
-                $text = sprintf('%s %sms %s', $query->connectionName, $query->time, $sql);
+                $text = sprintf('%s %sms:%s', $query->connectionName, $query->time, $sql);
                 Log::channel(config('faravel.listen_sql.log'))->info($text);
+            });
+        }
+    }
+
+    /**
+     * 监听 redis
+     */
+    protected function listenRedis()
+    {
+        foreach(config('faravel.listen_redis') as $connection => $log)
+        {
+            app('redis')->connection($connection)->enableEvents();
+            app('redis')->connection($connection)->listen(function(CommandExecuted $cmd) use($log) {
+                $text = sprintf("%s %dms:%s %s", $cmd->connectionName, $cmd->time, $cmd->command, implode(' ', $cmd->parameters));
+                Log::channel($log)->info($text);
             });
         }
     }
